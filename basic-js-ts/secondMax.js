6ae037cca2173a85ca7d9c53119e2bf45c9f346b
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-05 17:29:42 +0700

    second max

---
+++ basic-js-ts/secondMax.js
@@ -0,0 +1,12 @@
+function secondMax(arr) {
+  if (!arr.length) throw new Error("Error!");
+  else if (arr.length === 1) return arr;
+
+  arr.sort((a, b) => a - b);
+  return arr[arr.length - 2];
+}
+try {
+  console.log(secondMax([4123]));
+} catch (error) {
+  console.log(error.message);
+}
