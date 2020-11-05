dca6acf8a8156de7975d9131e520d89e5f685a1f
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-05 17:54:29 +0700

    fizz buzz

---
+++ basic-js-ts/fizzBuzz.js
@@ -0,0 +1,9 @@
+function fizzBuzz(num) {
+  return (
+    (num % 3 === 0 && num % 5 === 0 && "FizzBuzz") ||
+    (num % 3 === 0 && "Fizz") ||
+    (num % 5 === 0 && "Buzz")
+  );
+}
+
+console.log(fizzBuzz(25));
