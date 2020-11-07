10e35c9a3398655e779685748c1100e00b389163
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-11-07 14:23:56 +0700

    edit return arr[0] if length of array is equal 1

--- basic-js-ts/secondMax.js
+++ basic-js-ts/secondMax.js
@@ -1,6 +1,6 @@
 function secondMax(arr) {
   if (!arr.length) throw new Error("Error!");
-  else if (arr.length === 1) return arr;
+  else if (arr.length === 1) return arr[0];
 
   arr.sort((a, b) => a - b);
   return arr[arr.length - 2];
